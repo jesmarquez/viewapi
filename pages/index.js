ce3e99d933bd9cba32ef4b21a4c17877b5639a03
jesmarquez <jesmqz@gmail.com>
2019-03-06 14:50:59 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import "../styles.scss"
+
+function Home() {
+    return( 
+    <div className="container">
+    <div className="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
+    </div>
+    )
+  }
+  
+  export default Home
